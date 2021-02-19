fee94d1b79908a90f532e549ed31e86e16b86c40
Aline Lima <allinelima@gmail.com>
2021-02-19 00:41:54 -0300

    Funcao arquivo calc corrigido

--- calc.rb
+++ calc.rb
@@ -1,5 +1,4 @@
 print "Digite o primeiro número inteiro: "
-# .to_i transforma a string em um número inteiro
 number1 = gets.chomp.to_i
 print "Digite o segundo número inteiro: "
 number2 = gets.chomp.to_i
