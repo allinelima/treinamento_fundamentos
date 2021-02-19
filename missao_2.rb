2bbc198ebba5b909593af7d22304d8aea0bcb474
Aline Lima <allinelima@gmail.com>
2021-02-19 00:28:40 -0300

    Adicionando as Missoes

---
+++ missao_2.rb
@@ -0,0 +1,8 @@
+# Missão 2
+print 'Digite seu nome: '
+name = gets.chomp
+print 'Digite seu Sobrenome: '
+sobrenome = gets.chomp
+print 'Digite sua idade: '
+idade = gets.chomp
+puts "Hello #{name} #{sobrenome} #{idade} anos!"
