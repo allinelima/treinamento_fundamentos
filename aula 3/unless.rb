df5e71bcfb71057ba36ee987e7b9431c0020af92
Aline Lima <allinelima@gmail.com>
2021-02-19 01:51:42 -0300

    Inclusao de funcoes

--- aula 3/unless.rb
+++ aula 3/unless.rb
@@ -7,3 +7,14 @@ else
 end
 
 puts "You #{check_change} change the product"
+
+
+product_status = 'closed'
+
+if not product_status == 'open'
+  check_change = 'can'
+else
+  check_change = 'can not'
+end
+
+puts "You #{check_change} change the product"
